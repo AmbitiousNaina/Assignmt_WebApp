ad3543df3a1a017a47e4ce7dd4be70f45d81026f
AmbitiousNaina <74875626+AmbitiousNaina@users.noreply.github.com>
2020-12-05 15:12:10 +0530

    update changes

--- app.js
+++ app.js
@@ -4,29 +4,25 @@ var outputDiv = document.querySelector("#output");
 
 var serverUrl = "https://api.funtranslations.com/translate/ermahgerd.json"
 
-function getTranslationUrl(input)
-{
-    return serverUrl + "?" +"text=" + input
+function getTranslationUrl(input) {
+    return serverUrl + "?" + "text=" + input
 }
 
-function errorHandler(error)
-{
-    console.log("Error Occured", error);
-    alert("Try again later!!")
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("try again later")
 }
 
-function clickHandler()
-{
-var inputText = userInput.value; //taking an input
+function clickHandler() {
+    var inputText = userInput.value; //taking a value
 
-fetch(getTranslationUrl(inputText))
-.then(response => response.json())
-.then(json => {
-    var translatedText = json.contents.translated;
-   outputDiv.innerText = translatedText;
-})
-.catch(errorHandler)
-
+    //calling server for processing
+    fetch(getTranslationUrl(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; //output
+        })
+        .catch(errorHandler)
 };
-
-btnTranslate.addEventListener("click", clickHandler)
+btnTranslate.addEventListener("click", clickHandler);
